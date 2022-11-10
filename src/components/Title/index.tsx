ef990a3e6a610bc1760d69f4bc9ca04d36aa14ab
Redeemer <red.pace.dev@gmail.com>
2022-11-10 23:05:27 +0100

    feat: Added title component

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,3 @@
+import tw from "twin.macro";
+
+export const Title = tw.div`relative m-[0 0 30px 0] lg:m-[0 0 35px 35px] inline-block align-top text-2xl font-semibold	text-white transition-[all 0.3s ease 0s]`;
