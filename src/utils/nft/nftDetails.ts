988ceb0eb43500b30ce8e871768349dd0ae0c008
Redeemer <red.pace.dev@gmail.com>
2022-11-10 21:09:52 +0100

    chore: Removed asset type from nft details

--- src/utils/nft/nftDetails.ts
+++ src/utils/nft/nftDetails.ts
@@ -1,16 +1,14 @@
 import { alchemy } from "@/setups/alchemy";
-import { Lending, NftAssetType, ReadableNftItem, Renting } from "@/types/nft";
+import { Lending, ReadableNftItem, Renting } from "@/types/nft";
 import { toPaymentToken } from "@/utils/currency/tokens";
 
 export const getFullNftDetails = async (nft: Lending, renting?: Omit<Renting, "lending">): Promise<ReadableNftItem> => {
   const nftMetaData = await alchemy.nft.getNftMetadata(nft.nftAddress, nft.tokenId);
-  const assetUrl: string = nftMetaData.media?.at(0)?.gateway || "";
-  const assetType = nftMetaData.media?.at(0)?.format ? NftAssetType.IMAGE : NftAssetType.VIDEO;
+  const assetUrl: string = nftMetaData.rawMetadata?.image_url || nftMetaData.rawMetadata?.image;
 
   return {
     ...nft,
     assetUrl,
-    assetType,
     nftPrice: parseInt(nft.nftPrice || "", 16),
     dailyRentPrice: parseInt(nft.dailyRentPrice || "", 16),
     paymentTokenName: toPaymentToken(nft.paymentToken || -1),
