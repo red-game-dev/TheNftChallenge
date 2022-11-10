ae3c9251913fa59832cc701bb6fabe5e77304377
Redeemer <red.pace.dev@gmail.com>
2022-11-10 12:43:00 +0100

    feat: Added interfaces for our refctored objects of responses

---
+++ src/types/nft.ts
@@ -0,0 +1,46 @@
+import { Nft } from "alchemy-sdk";
+
+export enum NftAssetType {
+  IMAGE = "image",
+  VIDEO = "video",
+  AUDIO = "AUDIO"
+}
+
+export interface Lending {
+  id: string;
+  collateralClaimed: boolean;
+  cursor: number;
+  dailyRentPrice: string;
+  maxRentDuration: string;
+  isERC721: boolean;
+  lenderAddress: string;
+  lentAmount: string;
+  lentAt: string;
+  nftAddress: string;
+  nftPrice: string;
+  paymentToken: number;
+  tokenId: string;
+}
+
+export interface Renting {
+  id: string;
+  cursor: number;
+  rentDuration: string;
+  renterAddress: string;
+  lending: Lending;
+}
+
+export interface AzrealNFTS {
+  lendings: Lending[];
+  rentings: Renting[];
+}
+
+export type ReadableNftItem = Omit<Lending, "nftPrice" | "dailyRentPrice"> & {
+  nftPrice: number;
+  dailyRentPrice: number;
+  paymentTokenName: string;
+  assetUrl: string;
+  assetType: NftAssetType;
+} & Nft & {
+  renting?: Omit<Renting, "lending">;
+};
