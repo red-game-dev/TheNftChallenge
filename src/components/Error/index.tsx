c5292b4d27f8067150c0657eceb73723c41e2fdf
Redeemer <red.pace.dev@gmail.com>
2022-11-10 18:27:34 +0100

    feat: Added an error component

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,19 @@
+import tw from "twin.macro";
+
+const ErrorContainer = tw.div`bg-red-50 border-l-8 border-red-700 mb-2`;
+const ErrorCotent = tw.div`flex items-center p-2`;
+const Icon = tw.span`ml-2 text-red-700`;
+const Paragraph = tw.p`px-6 py-4 text-red-700 font-semibold text-lg`;
+
+interface ErrorDetailProps {
+  text: string;
+}
+
+export const ErrorDetail = ({ text }: ErrorDetailProps) => (
+  <ErrorContainer>
+      <ErrorCotent>
+        <Icon className="fa-duotone fa-circle-xmark" />
+        <Paragraph>{text}</Paragraph>
+      </ErrorCotent>
+  </ErrorContainer>
+);
