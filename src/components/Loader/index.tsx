bd646a83b1a1b1e8533a4282385d6484c7d45edd
Redeemer <red.pace.dev@gmail.com>
2022-11-10 23:05:19 +0100

    feat: Added loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,3 @@
+import tw from "twin.macro";
+
+export const Loader = tw.svg`w-[30px] h-[30px] rounded-[100%] rounded-bl-[50%] rounded-tr-[50%] border-[1px] border-b-dashed p-8 border-[#4bffa5] animate-spin m-auto`;
